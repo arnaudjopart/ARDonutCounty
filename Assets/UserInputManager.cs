42362bcd8881f08ec11539eee010890be36f9d25
Arnaud Jopart <arnaudjopart@users.noreply.github.com>
2023-10-18 00:00:46 +0200

    XR environment

--- Assets/UserInputManager.cs
+++ Assets/UserInputManager.cs
@@ -3,29 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
-public class UserInputManager : MonoBehaviour
+namespace Ajc
 {
-    [SerializeField] private ARRaycastManager m_raycastManager;
-    [SerializeField] private GameObject m_holePrefab;
-    // Start is called before the first frame update
-    void Start()
+    public class UserInputManager : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private ARRaycastManager m_raycastManager;
+        [SerializeField] private GameObject m_holePrefab;
+        private string m_name;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+
+        // Start is called before the first frame update
+        void Start()
         {
-            Vector2 finger_0_position = Input.GetTouch(0).position;
-            var listOfHits = new List<ARRaycastHit>();
-            if (m_raycastManager.Raycast(finger_0_position, listOfHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
             {
-                var hit = listOfHits[0];
-                var positionOfHit = hit.pose.position;
-                Instantiate(m_holePrefab, positionOfHit, Quaternion.identity);
+                Vector2 finger_0_position = Input.GetTouch(0).position;
+                var listOfHits = new List<ARRaycastHit>();
+                if (m_raycastManager.Raycast(finger_0_position, listOfHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    var hit = listOfHits[0];
+                    var positionOfHit = hit.pose.position;
+                    Instantiate(m_holePrefab, positionOfHit, Quaternion.identity);
+                }
             }
-        }  
+        }
+
+        private string ReturnGameObjectName(GameObject _gameObject)
+        {
+            m_name = _gameObject.name;
+            return m_name;
+        }
+
+        private bool IsActive()
+        {
+            return gameObject.activeSelf;
+        }
     }
 }
+
+
