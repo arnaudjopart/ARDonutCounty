a24257d067076d0ec56519766b6da3c46ea5541a
thomasbon2000 <thomasbon2000@gmail.com>
2023-10-20 13:47:37 +0200

    Revert "Merge branch 'GameMechanism' into Thomas"
    This reverts commit 0e3140a9a1d41f1f5dc38b00c6c1579e43eed588, reversing
    changes made to 11a517efaeddf8d8e38ca9c3f5e22f6fd5fd13b4.

--- Assets/_Project/Scripts/UserInputManager.cs
+++ Assets/_Project/Scripts/UserInputManager.cs
@@ -1,19 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
-using DG.Tweening;
-using UnityEngine.XR.Interaction.Toolkit.Utilities.Tweenables.Primitives;
-
-namespace Christophe.Fanchamps { 
 
 public class UserInputManager : MonoBehaviour
 {
     [SerializeField] private ARRaycastManager m_raycastManager;
-    [SerializeField] private GameObject m_holePrefab, m_cubePrefab;
-    private string m_name;
-    GameObject m_hole;
-    //[SerializeField] float m_speed;
+    [SerializeField] private DoorController m_controller;
+    [SerializeField] private GameObject m_cubeDebugPrefab;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,51 +18,65 @@ public class UserInputManager : MonoBehaviour
        
     }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                var position = new Vector2();
+            if (IsClickingOnUIElement()) return;
+            var position = new Vector2();
 #if UNITY_EDITOR
-                position = Input.mousePosition;
+            position = Input.mousePosition;
 #else
             position = Input.GetTouch(0).position;
 #endif
 
-                var listOfHits = new List<ARRaycastHit>();
-                if (m_raycastManager.Raycast(position, listOfHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
-                {
-                    var hit = listOfHits[0];
-                    var positionOfHit = hit.pose.position;
-                    if (m_hole == null)
-                    {
-                        m_hole = Instantiate(m_holePrefab, positionOfHit, Quaternion.identity);
-                        GetComponent<SwipeController>().enabled = true;
-                    }
-                    //else
-                    //{
-
-                    //    Instantiate(m_cubePrefab, positionOfHit + Vector3.up * .8f, Quaternion.identity);
-
-                    //}
-
-
-                }
+            var listOfHits = new List<ARRaycastHit>();
+            if (m_raycastManager.Raycast(position, listOfHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+            {
+                var hit = listOfHits[0];
+                var positionOfHit = hit.pose.position;
+                m_controller.ProcessTouch(positionOfHit);
+                
             }
-
-
         }
 
-    private string ReturnGameObjectName(GameObject _gameObject)
-    {
-        m_name = _gameObject.name;
-        return m_name;
+        if (Input.GetMouseButtonDown(1))
+        {
+            var position = new Vector2();
+#if UNITY_EDITOR
+            position = Input.mousePosition;
+#else
+            position = Input.GetTouch(0).position;
+#endif
+
+            var listOfHits = new List<ARRaycastHit>();
+            if (m_raycastManager.Raycast(position, listOfHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+            {
+                var hit = listOfHits[0];
+                var positionOfHit = hit.pose.position;
+                Instantiate(m_cubeDebugPrefab, positionOfHit + Vector3.up * .8f, Quaternion.identity);
+
+            }
+        }
     }
 
-    private bool IsActive()
+    private bool IsClickingOnUIElement()
     {
-        return gameObject.activeSelf;
+        return EventSystem.current.IsPointerOverGameObject(); 
     }
-}
+
+    public void GenerateCubeAtViewPortPosition()
+    {
+        var position = Camera.main.ViewportToScreenPoint(new Vector2(.5f, .5f));
+
+        var listOfHits = new List<ARRaycastHit>();
+        if (m_raycastManager.Raycast(position, listOfHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            var hit = listOfHits[0];
+            var positionOfHit = hit.pose.position;
+            Instantiate(m_cubeDebugPrefab, positionOfHit + Vector3.up * .8f, Quaternion.identity);
+
+        }
+    }
 }
